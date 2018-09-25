09eb413cd9d10ebb22db8d2bf11a58646c3a9025
TongAJ <875334300@qq.com>
2018-09-25 10:04:06 +0800

    adapter learning

---
+++ src/com/design/mode/adaptee/quesheng/InterfaceService.java
@@ -0,0 +1,25 @@
+package com.design.mode.adaptee.quesheng;
+
+/**
+ * 描述:
+ * 接口类：定义大量方法
+ *
+ * @author Administrator
+ * @create 2018-09-25 9:43
+ */
+public interface InterfaceService {
+
+    /**
+     * 方法1
+     */
+    public void serviceMethod01();
+    /**
+     * 方法2
+     */
+    public void serviceMethod02();
+    /**
+     * 方法3
+     */
+    public void serviceMethod03();
+
+}
